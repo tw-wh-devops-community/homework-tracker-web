3a24fa2bada31a3f60dc10dbb1028b574d15e8b6
Panpan Han <hanpanpan200@gmail.com>
2018-01-24 17:41:34 +0800

    [panpan] fix: remove duplicate line

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
 import { configure } from 'enzyme' // eslint-disable-line
 import Adapter from 'enzyme-adapter-react-16' // eslint-disable-line
 import 'jest-enzyme' // eslint-disable-line
-import 'jest-enzyme' // eslint-disable-line
 
 configure({ adapter: new Adapter() })
