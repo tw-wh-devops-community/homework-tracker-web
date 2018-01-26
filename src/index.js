c554b1a89e87c1bc2b008689af73ca9eaabbc133
Panpan Han <hanpanpan200@gmail.com>
2018-01-26 15:13:07 +0800

    [panpan] feat: update router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import AdminContainer from './homework/admin/Admin'
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <AdminContainer />
+    <Router>
+      <Route exact path="/admin" component={AdminContainer} />
+    </Router>
   </Provider>,
   document.getElementById('root'),
 )
