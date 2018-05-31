a51abfaa4f00e06034c0b6c3f076b19b6eb62a42
Xiaolong Long <xllong@CNxllong.local>
2018-05-31 18:00:04 +0800

    [xllong] home page toggle link color

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,22 +6,35 @@ import './Home.css'
 import Admin from '../admin/Admin'
 import InterviewerAdmin from '../interviewer/InterviewerAdmin'
 
-export const Home = () => (
-  <Router>
-    <div className="demo">
-      <div className="demo-nav">
-        <div className="homepage-logo">HOMEWORK 管理平台</div>
-        <Link to="/interviewer" replace>面试官管理</Link>
-        <span>|</span>
-        <Link to="/admin" replace>作业分配管理</Link>
-      </div>
-      <Switch>
-        <Route path="/admin" component={Admin} />
-        <Route path="/interviewer" component={InterviewerAdmin} />
-      </Switch>
-    </div>
-  </Router>
-)
+export class Home extends React.Component {
+  state = {
+    selected: 'admin',
+  }
+
+  changeClass = (name) => {
+    this.setState({ selected: name })
+  }
+
+  render() {
+    const { selected } = this.state
+    return (
+      <Router>
+        <div className="demo">
+          <div className="demo-nav">
+            <div className="homepage-logo">HOMEWORK 管理平台</div>
+            <Link to="/interviewer" replace className={selected === 'interviewer' ? 'selected' : 'unselected'} onClick={() => this.changeClass('interviewer')}>面试官管理</Link>
+            <span>|</span>
+            <Link to="/" replace className={selected === 'admin' ? 'selected' : 'unselected'} onClick={() => this.changeClass('admin')}>作业分配管理</Link>
+          </div>
+          <Switch>
+            <Route exact path="/" component={Admin} />
+            <Route path="/interviewer" component={InterviewerAdmin} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
 
 export default withRouter(Home)
 
